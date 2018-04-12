29bdaaf1bbf1f6595ba19705642687a250605d5f
Fred Barker <fb@fredbarker.com>
2018-04-11 22:44:25 -0700

    Design finished!

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,7 +1,8 @@
+// TOGGLE THE FLIPPED CLASS ON AND OFF
+
 function toggleClass (event) {
   event.target.parentNode.classList.toggle('flipped')
 }
 
 const deck = document.querySelector('.deck')
-
 deck.addEventListener('click', toggleClass)
